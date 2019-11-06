9a95d373143cb6f1be05331f095046de5c73021a
Yehor Hrabyna <grabunayegor@gmail.com>
2019-11-06 21:05:19 +0200

    upgrade-magento-grunt

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+    yehorh_luma_en_US: {
+        area: 'frontend',
+        name: 'Yehorh/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    yehorh_luma_uk_UA: {
+        area: 'frontend',
+        name: 'Yehorh/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    yehorh_luma_ru_RU: {
+        area: 'frontend',
+        name: 'Yehorh/luma',
+        locale: 'ru_RU',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
