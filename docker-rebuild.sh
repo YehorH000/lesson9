52b63064302ffdcb9448fd3b5d29effaf5c4fa7e
Yehor Hrabyna <grabunayegor@gmail.com>
2019-11-11 18:10:28 +0200

    first db

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -9,3 +9,4 @@ docker volume prune -f
 docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d --build --force-recreate
 # docker-compose -f ./docker-compose.yml -f ./docker-compose-dev.yml up -d
 # docker-sync-stack start
+grunt clean:yehorh_luma_en_US && grunt exec:yehorh_luma_en_US && grunt less:yehorh_luma_en_US && grunt watch
